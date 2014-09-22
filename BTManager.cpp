31abc091e6ee8f54ced5116b6cf4ce36dfb77f2f
中塩成海 <narumi18wa@gmail.com>
2014-09-22 13:51:16 +0900

    commit

--- BTManager.cpp
+++ BTManager.cpp
@@ -30,37 +30,55 @@ void BTManager::begin() {
 
 	btSerial.print("AT\r");
 	Serial.println("AT");
+	Serial.println(readMessage());
 
 	delay(1500);
 
 	btSerial.print("ATZ\r");
 	Serial.println("ATZ");
+	Serial.println(readMessage());
 
 	delay(1500);
 
 	btSerial.print("AT+BTNAME=" + BTName + "\r");
 	Serial.println("AT+BTNAME=" + BTName + "\r");
+	Serial.println(readMessage());
 
 	delay(1500);
 
 	btSerial.print("AT+BTKEY=1234\r");
 	Serial.println("AT+BTKEY=1234");
+	Serial.println(readMessage());
 
 	delay(1500);
 
 	btSerial.print("AT+BTSCAN\r");
 	Serial.println("AT+BTSCAN");
+	Serial.println(readMessage());
 
 	delay(1500);
 
 	btSerial.print("AT+BTINFO?0\r");
 	Serial.println("AT+BTINFO?0");
+	Serial.println(readMessage());
 
 	delay(1500);
 
 	Serial.println("Sterted Device.");
 }
 
-String BTManager::readMessage(){
+String BTManager::readMessage() {
+	String str = "";
+	while (1) {
+		int timeoutCount = 0;
 
+		while (!btSerial.available()) {
+			if (timeoutCount > 50) {
+				return str;
+			}
+			timeoutCount++;
+			delay(100);
+		}
+		str += btSerial.read();
+	}
 }
