e5810b6f3f122e352c1d46332ab366b728096b4c
中塩成海 <narumi18wa@gmail.com>
2014-09-22 22:07:24 +0900

    エラー解決

--- BTManager.cpp
+++ BTManager.cpp
@@ -21,58 +21,48 @@ BTManager::BTManager(String BTName) {
 }
 
 void BTManager::begin() {
-	Serial.println("Starting Device...");
 
 	delay(1500);
 	btSerial.print(esc);
-	Serial.println("esc");
 	delay(1500);
 
 	btSerial.print("AT\r");
-	Serial.println("AT");
 	readMessage();
 	delay(1500);
 
 	btSerial.print("ATZ\r");
-	Serial.println("ATZ");
 	readMessage();
 	delay(1500);
 
 	btSerial.print("AT+BTNAME=" + BTName + "\r");
-	Serial.println("AT+BTNAME=" + BTName + "\r");
 	readMessage();
 	delay(1500);
 
 	btSerial.print("AT+BTKEY=1234\r");
-	Serial.println("AT+BTKEY=1234");
 	readMessage();
 	delay(1500);
 
 	btSerial.print("AT+BTSCAN\r");
-	Serial.println("AT+BTSCAN");
 	readMessage();
 	delay(1500);
 
 	btSerial.print("AT+BTINFO?0\r");
-	Serial.println("AT+BTINFO?0");
 	readMessage();
 	delay(1500);
-
-	Serial.println("Sterted Device.");
 }
 
-void BTManager::readMessage() {
+String BTManager::readMessage() {
 	String message = "";
 	char c;
 
 	while (btSerial.available() == 0) {
 	}
 
-	while(btSerial.available() > 0){
+	while (btSerial.available() > 0) {
 		c = btSerial.read();
 		message += String(c);
 	}
 
-	Serial.println(message);
+	return message;
 }
 
--- BTManager.h
+++ BTManager.h
@@ -29,7 +29,7 @@ public:
 	bool isConnected();
 	bool isPaired();
 
-	void readMessage();
+	String readMessage();
 	bool writeMessage(String message);
 };
 
