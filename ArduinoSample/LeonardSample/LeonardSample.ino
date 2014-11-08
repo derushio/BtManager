f843de52494ac4947c98b90ba9c467590210f6f2
中塩成海 <narumi18wa@gmail.com>
2014-11-08 19:41:05 +0900

    サンプルを大幅に変更

--- ArduinoSample/LeonardSample/LeonardSample.ino
+++ ArduinoSample/LeonardSample/LeonardSample.ino
@@ -3,6 +3,89 @@
 
 BTManager *btManager;
 
+void mouseMove(int xMove, int yMove, int scroll) {
+    while (xMove != 0 || yMove != 0 || scroll != 0) {
+        int x = 0;
+        int y = 0;
+        int s = 0;
+
+        if (xMove > 0) {
+            x = 1;
+            xMove--;
+        } else if (xMove < 0) {
+            x = -1;
+            xMove++;
+        }
+
+        if (yMove > 0) {
+            y = 1;
+            yMove--;
+        } else if (yMove < 0) {
+            y = -1;
+            yMove++;
+        }
+
+        if (scroll > 0) {
+            s = 1;
+            scroll--;
+        } else if (scroll < 0) {
+            s = -1;
+            scroll++;
+        }
+
+        Mouse.move(x, y, s);
+
+    }
+}
+
+void mouseAction(int mouseActionFlag) {
+    switch (mouseActionFlag) {
+        case 0:
+            break;
+        case 1:
+            Mouse.click();
+            break;
+    }
+}
+
+void controlXYSM(String message) {
+    int codeLength = 16;
+    char code[codeLength];
+
+    int xIndex = message.indexOf("x");
+    int yIndex = message.indexOf("y");
+    int sIndex = message.indexOf("s");
+    int mIndex = message.indexOf("m");
+    int endIndex = message.indexOf("e");
+
+    long xMove = 0;
+    long yMove = 0;
+    long scroll = 0;
+    int mouseActionFlag = 0;
+
+    if (xIndex != -1 || yIndex != -1 || sIndex != -1 || mIndex != -1 || endIndex != -1) {
+        message.substring(xIndex + 1, yIndex).toCharArray(code, codeLength);
+        xMove = atoi(code);
+        Serial.println(xMove);
+
+        message.substring(yIndex + 1, sIndex).toCharArray(code, codeLength);
+        yMove = atoi(code);
+        Serial.println(yMove);
+
+        message.substring(sIndex + 1, mIndex).toCharArray(code, codeLength);
+        scroll = atoi(code);
+        Serial.println(scroll);
+
+        message.substring(mIndex + 1, endIndex).toCharArray(code, codeLength);
+        mouseActionFlag = atoi(code);
+        Serial.println(mouseActionFlag);
+
+    }
+
+    mouseMove(xMove, yMove, scroll);
+    mouseAction(mouseActionFlag);
+}
+
 void setup() {
     Serial.begin(9600);
     Serial.println("Device Starting...");
@@ -17,63 +100,15 @@ void loop() {
     String message = btManager->readMessage();
     if (message != "") {
         Serial.println(message);
-        int codeLength = 16;
-        char code[codeLength];
 
-        int xIndex = message.indexOf("x");
-        int yIndex = message.indexOf("y");
-        int sIndex = message.indexOf("s");
-        int endIndex = message.indexOf("e");
+        int actIndex = message.indexOf("a");
 
-        long xMove = 0;
-        long yMove = 0;
-        long scroll = 0;
+        if (actIndex != -1) {
+            // actによる制御
 
-        if (xIndex != -1 || yIndex != -1 || sIndex != -1 || endIndex != -1) {
-            message.substring(xIndex + 1, yIndex).toCharArray(code, codeLength);
-            xMove = atoi(code);
-            Serial.println(xMove);
-
-            message.substring(yIndex + 1, sIndex).toCharArray(code, codeLength);
-            yMove = atoi(code);
-            Serial.println(yMove);
-
-            message.substring(sIndex + 1, endIndex).toCharArray(code, codeLength);
-            scroll = atoi(code);
-            Serial.println(scroll);
-
-            while (xMove != 0 || yMove != 0 || scroll != 0) {
-                int x = 0;
-                int y = 0;
-                int s = 0;
-
-                if (xMove > 0) {
-                    x = 1;
-                    xMove--;
-                } else if (xMove < 0) {
-                    x = -1;
-                    xMove++;
-                }
-
-                if (yMove > 0) {
-                    y = 1;
-                    yMove--;
-                } else if (yMove < 0) {
-                    y = -1;
-                    yMove++;
-                }
-
-                if (scroll > 0) {
-                    s = 1;
-                    scroll--;
-                } else if (scroll < 0) {
-                    s = -1;
-                    scroll++;
-                }
-
-                Mouse.move(x, y, s);
-
-            }
+        } else {
+            // xysmによる制御
+            controlXYSM(message);
         }
     }
 }
